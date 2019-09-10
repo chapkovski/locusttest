933a70ed570326d031e3654ca07b5ebc2f7fc636
chapkovski <chapkovski@gmail.com>
2019-09-10 14:53:29 -0400

    rolling back

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-worker: python locust.py
-#worker: locust --host=$TESTHOST:$PORT --locustfile locust.py
+worker: locust --host=$TESTHOST:$PORT --locustfile locustrunner.py
--- locustrunner.py
+++ locustrunner.py
@@ -1,20 +1,20 @@
-print('PIZDA')
 
-# from locust import HttpLocust, TaskSet, task
-#
-#
-# class UserBehavior(TaskSet):
-#
-#     @task(1)
-#     def list_post(self):
-#         headers = {'content-type': 'application/json', 'Accept-Encoding': 'gzip'}
-#         a = self.client.post("/model/parse",
-#                              headers=headers,
-#                              name="access nlu",
-#                              data='{"text":"hello"}')
-#
-#         print(a.status_code)
-#
-#
-# class WebsiteUser(HttpLocust):
-#     task_set = UserBehavior
+
+from locust import HttpLocust, TaskSet, task
+
+
+class UserBehavior(TaskSet):
+
+    @task(1)
+    def list_post(self):
+        headers = {'content-type': 'application/json', 'Accept-Encoding': 'gzip'}
+        a = self.client.post("/model/parse",
+                             headers=headers,
+                             name="access nlu",
+                             data='{"text":"hello"}')
+
+        print(a.status_code)
+
+
+class WebsiteUser(HttpLocust):
+    task_set = UserBehavior
